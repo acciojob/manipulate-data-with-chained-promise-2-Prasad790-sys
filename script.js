eafdb8d1d78482673af446156b413c61ffd626d7
Prasad790-sys <psa19974@gmail.com>
2024-10-03 21:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ getNumbers()
         // Filter out odd numbers and update output after 1 second
         const evenNumbers = numbers.filter(num => num % 2 === 0);
         setTimeout(() => {
-            document.getElementById('output').textContent = `${evenNumbers.join(', ')}`;
+            document.getElementById('output').textContent = `${evenNumbers.join(',')}`;
         }, 1000);
         
         // Return the even numbers to chain the next promise
@@ -20,6 +20,6 @@ getNumbers()
         // Multiply even numbers by 2 and update output after 2 second
         const multipliedNumbers = evenNumbers.map(num => num * 2);
         setTimeout(() => {
-            document.getElementById('output').textContent = `${multipliedNumbers.join(', ')}`;
+            document.getElementById('output').textContent = `${multipliedNumbers.join(',')}`;
         }, 2000);
     });
